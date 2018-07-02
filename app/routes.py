48246b33e1a9d51f68b3564e20df5fb17b656cf7
salesvictor <salesrvictor@gmail.com>
2018-07-02 00:37:48 -0300

    file listing working

--- app/routes.py
+++ app/routes.py
@@ -1,33 +1,81 @@
-from app import app
-from flask import render_template, request, redirect, url_for, make_response, session
+from flask import render_template, request, redirect, url_for, make_response, session, send_from_directory
+from werkzeug.utils import secure_filename
+from . import app
+import os
+
+@app.route('/download/<path:filename>')
+def download(filename):
+  return send_from_directory(os.path.join(os.path.pardir, app.config['UPLOAD_FOLDER']), filename)
 
 @app.route('/')
 def index():
-  if not 'username' in session or not request.cookies.get('username'):
-    return render_template('index.html')
+  if 'username' in session or 'username' in request.cookies:
+    return redirect(url_for('home'))
 
-  return redirect(url_for('store'))
+  return render_template('index.html')
 
 @app.route('/cookies', methods=['GET', 'POST'])
 def cookies():
+  if session.get('username'):
+    return redirect(url_for('home'))
+
   if request.method == 'POST':
     username = request.form.get('username')
 
-    response = make_response(redirect(url_for('store')))
+    response = make_response(redirect(url_for('home')))
     response.set_cookie('username', username)
 
     return response
 
-  return render_template('form.html')
+  return render_template('login_form.html')
 
 @app.route('/sessions', methods=['GET', 'POST'])
 def sessions():
+  if request.cookies.get('username'):
+    return redirect(url_for('home'))
+
   if request.method == 'POST':
     session['username'] = request.form.get('username')
-    return redirect(url_for('store'))
+    return redirect(url_for('home'))
 
-  return render_template('form.html')
+  return render_template('login_form.html')
 
-@app.route('/store')
-def store():
-  return 'store'
+@app.route('/home')
+def home():
+  username = request.cookies.get('username') or session.get('username')
+
+  if not username:
+    return redirect(url_for('index'))
+
+  return render_template('home.html', username=username)
+
+@app.route('/logout')
+def logout():
+  response = make_response(redirect(url_for('index')))
+
+  session.pop('username', None)
+  response.delete_cookie('username')
+    
+  return response
+
+def allowed_file(filename):
+  return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_files():
+  if request.method == 'POST':
+    uploaded_files = request.files.getlist('files')
+
+    for uploaded_file in uploaded_files:
+      if allowed_file(uploaded_file.filename):
+        filename = secure_filename(uploaded_file.filename)
+        uploaded_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+    return redirect(url_for('view_files'))
+
+  return render_template('upload_form.html')
+
+@app.route('/files')
+def view_files():
+  files = os.listdir(app.config['UPLOAD_FOLDER'])
+  return render_template('view_files.html', files=files)
--- config.py
+++ config.py
@@ -2,3 +2,5 @@ import os
 
 class Config:
   SECRET_KEY = os.environ.get('SECRET_KEY') or 'huehue-brbr'
+  ALLOWED_EXTENSIONS = set(['txt', 'png', 'mov', 'mp3', 'mp4', 'webm', 'pdf', 'jpg', 'jpeg'])
+  UPLOAD_FOLDER = os.path.join('app', 'uploads')
